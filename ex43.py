9c4fa7236681f17ee58981f0ed465a4152871cc2
Aaron G <aaron@navarc.co>
2021-12-23 15:11:55 -0600

    learned about game engine design and the way to structure objects

---
+++ ex43.py
@@ -0,0 +1,7 @@
+#basic object-oriented Analysis and Design
+"""
+1. write or draw about the prodblem
+2. Extract key concepts from 1 and research them.
+3. create a class hierarchy and object map for hte concepts.
+4. Code the classes and a test to run them.
+5. Repeat and refine."""
